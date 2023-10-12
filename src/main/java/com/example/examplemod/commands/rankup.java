1f49f0a9befad2f10d0e8ac4e435712d1ee9ad1d
Arberx9 <arber.kulla888@gmail.com>
2023-10-12 11:54:44 +0200

    ello

---
+++ src/main/java/com/example/examplemod/commands/rankup.java
@@ -0,0 +1,14 @@
+package com.example.examplemod.commands;
+
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.server.MinecraftServer;
+
+public class rankup extends CommandBase {
+
+
+    public void execute(MinecraftServer server, ICommandSender player, String[] args) {
+    //Im working on this still
+    }
+
+}
